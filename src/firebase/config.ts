50aa2842b99f94038aff5672d7697750217fa3fa
hschinmaybharadwaj <chinmayabharadwajhs@CHINMAYAs-MacBook-Air.local>
2026-05-31 22:20:57 +0530

    updated

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-5562087172-a4c9e",
   "appId": "1:531691224178:web:6857f11ea01ae2ed3c16eb",
-  "apiKey": "AIzaSyCdduh6J4h6MKem3Et-fFUlL2b1EOklYO0",
+  "apiKey": "GEMINI-API-KEY",
   "authDomain": "studio-5562087172-a4c9e.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "531691224178"
